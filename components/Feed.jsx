d81d3dac0ff5c1e6a7b41e5bd39c68c109ff83d6
Amod <amodnagbhidkar08@gmail.com>
2023-07-20 20:26:46 +0530

    File

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -37,7 +37,7 @@ const Feed = () => {
 
   return (
     <section className="feed">
-      <form className="relative w-full flex-center">
+      {/* <form className="relative w-full flex-center">
         <input
           type="text"
           placeholder="Search for a tag or a username"
@@ -46,7 +46,7 @@ const Feed = () => {
           required
           className="search_input peer"
         />
-      </form>
+      </form> */}
 
       <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
